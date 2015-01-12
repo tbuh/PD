bdd14b7852560ba79410d924272057c947eda85b
Taras Buha <taras.buha@gmail.com>
2015-01-12 17:08:38 +0200

    Added sample repo and context

---
+++ WebPD.API/Entities/EntitiesContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace WebPD.API.Entities
+{
+    public class EntitiesContext : DbContext
+    {
+        public EntitiesContext()
+            : base(GetConnection("DefaultConnection"), contextOwnsConnection: true)
+        {
+        }
+
+        private static DbConnection GetConnection(string connectionStringName)
+        {
+            var setting = ConfigurationManager.ConnectionStrings[connectionStringName];
+            var connection = new SqlConnection(setting.ConnectionString);
+            return connection;
+        }
+    }
+}
---
+++ WebPD.API/Entities/Repositories/EntityRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebPD.API.Entities.Repositories
+{
+    public class EntityRepository
+    {
+        protected EntitiesContext _entitiesContext;
+        public EntityRepository()
+        {
+            _entitiesContext = new EntitiesContext();
+        }
+    }
+}
