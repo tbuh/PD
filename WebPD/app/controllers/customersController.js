0727a41f187a01f3a8edfc5ea7323b5b66fbefc0
Jack-Swan <mrJackSwan@gmail.com>
2015-01-14 12:56:12 +0200

    Delete old code from customer view

--- WebPD/app/controllers/customersController.js
+++ WebPD/app/controllers/customersController.js
@@ -6,26 +6,6 @@ app.controller('customersController', ['$scope', 'customersService', function ($
     customersService.getCustomers().then(function (results) {
 
         $scope.customers = results.data;
-        $scope.myData = [
-    {
-        "firstName": "Cox",
-        "lastName": "Carney",
-        "company": "Enormo",
-        "employed": true
-    },
-    {
-        "firstName": "Lorraine",
-        "lastName": "Wise",
-        "company": "Comveyer",
-        "employed": false
-    },
-    {
-        "firstName": "Nancy",
-        "lastName": "Waters",
-        "company": "Fuelton",
-        "employed": false
-    }
-        ];
 
     }, function (error) {
         //alert(error.data.message);
